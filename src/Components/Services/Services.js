c011bfa5324688baeb9252bea5bb16fb822224dc
razon1494 <razon1494@gmail.com>
2021-10-29 15:29:28 +0600

    spinner added

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,16 +1,25 @@
 import React, {useEffect, useState} from 'react';
+import {Spinner} from 'react-bootstrap';
 import Service from '../Service/Service';
 import './Services.css';
 const Services=() => {
     const [services, setServices]=useState([]);
+    const [loading, setLoading]=useState(true);
         useEffect(() => {
         fetch('http://localhost:5000/services')
             .then(res => res.json())
-            .then(data => setServices(data));
-    }, [])
+            .then(data => {
+                setServices(data);
+                setLoading(false)
+            });
+        }, [])
+    console.log(services)
     return (
         <div id="services">
             <h2 className="text-primary mt-5">Our services</h2>
+            {
+                loading && <Spinner animation="grow" variant="warning" />
+            }
             <div className="container row mx-auto">
                 {
                     services.map(service => <Service
